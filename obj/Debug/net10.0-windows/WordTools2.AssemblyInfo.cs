1fa1ad92b3fe3e6389d5309396a5b866c4c6c9c7
freeskychenjun <95356671@qq.com>
2025-12-26 22:55:50 +0800

    清理测试文件：删除所有测试相关代码和项目

--- obj/Debug/net10.0-windows/WordTools2.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/WordTools2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordTools2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f81a04f91544c3d007e1d878f02fbf54283265")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordTools2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordTools2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
